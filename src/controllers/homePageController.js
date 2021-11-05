fd9e0ba52cc3004adf105a7bcba254acde56f86c
ITstudentNiels <460499@student.fontys.nl>
2021-11-05 14:19:19 +0100

    Version 2

--- src/controllers/homePageController.js
+++ src/controllers/homePageController.js
@@ -5,7 +5,6 @@ import checkLogin from "../services/loginCheck";
 let handleHomepage = async (req, res) => {
     //Variable that contains information about the instance from the current user
     let instanceInfo = await instanceService.getInstance(req.user.id);
-<<<<<<< HEAD
     //If the information is empty set the variable to empty information
     if (!instanceInfo) {
         instanceInfo = {
@@ -34,35 +33,6 @@ let handleHomepage = async (req, res) => {
         info: instanceInfo,
         alert: alertMessage,
     })
-=======
-    await checkLogin.check(req.user.id, req.user.student_id);
-    if (!instanceInfo) {
-	instanceInfo = {
-		domain_name: '',
-		valid_until: '',
-		files_uploaded: '',
-		instance_type: '',
-		database: ''
-	}
-    }
-    let error = req.flash("errors");
-    error.push("&times;");
-	console.log(error);
-    if (Object.keys(error).length <= 1) {
-	error = ["", ""];
-    } 
-    //let errors;
-    //if (typeof errors == 'undefined') {
-    //    errors = ["Welcome " + req.user.fullname];
-    //}
-//    console.log(req);
-    return await res.render("homepage.ejs", {
-	user: req.user,
-	info: instanceInfo,
-	//errors: ["Welcome"]
-        errors: error,
-    });
->>>>>>> 99fb57ed01f86315e6c19a719d8e03a94719a3ac
 };
 
 module.exports = {
