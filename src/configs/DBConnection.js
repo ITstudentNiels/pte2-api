ea14dab5b73be8264b5b121084416bdda650e0d0
ITstudentNiels <460499@student.fontys.nl>
2021-11-05 14:08:37 +0100

    Version 2

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -6,21 +6,8 @@ let connection = mysql.createConnection({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-<<<<<<< HEAD
-    multipleStatements: true,
-=======
     multipleStatements: true
->>>>>>> 99fb57ed01f86315e6c19a719d8e03a94719a3ac
 });
-
-//let connection = mysql.createPool({
-//	host: process.env.DB_host,
-//	user: process.env.DB_USERNAME,
-//	password: process.env.DB_PASSWORD,
-//	database: process.env.DB_NAME,
-//	multipleStatements: true,
-//	connectionLimit: 10,
-//});
 
 connection.connect(function(err) {
     if (err) throw err;
