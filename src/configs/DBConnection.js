ba4929cd9026d796c23f47a8b3a2a9bed9f7f09b
ITstudentNiels <460499@student.fontys.nl>
2021-11-08 08:47:37 +0100

    Version 2

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 import mysql from "mysql2";
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
+    connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    rowsAsArray: false,
     multipleStatements: true
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Database connected!");
-});
+})
 
 module.exports = connection;
